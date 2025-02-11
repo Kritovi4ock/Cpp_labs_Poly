31c3829b11b10178028e1787257893916fe8c153
Егор Коваль <egor.koval.2005.1@gmail.com>
2025-02-11 12:27:57 +0300

    lab1

--- lab1/quadratic.h
+++ lab1/quadratic.h
@@ -2,8 +2,8 @@
 #ifndef QUADRATIC_H
 #define QUADRATIC_H
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 void solve_equation(double a, double b, double c);
 double discriminant(double a, double b, double c);
