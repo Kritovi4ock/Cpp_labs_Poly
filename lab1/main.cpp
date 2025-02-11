ecebdd00938f9ae7de41e8a28610ddef9324697b
Егор Коваль <egor.koval.2005.1@gmail.com>
2025-02-11 12:11:18 +0300

    lab1

---
+++ lab1/main.cpp
@@ -0,0 +1,9 @@
+#include "quadratic.h"
+int main() 
+{
+    double a, b, c;
+    std::cout << "Enter the coefficients a, b, c of a quadratic equation of the form: ax^2+bx+c=0" << std::endl;
+    std::cin >> a >> b >> c;
+    solve_equation(a, b, c);
+    return 0;
+}
